96b58ae9e1975322f6122eb8098f0d54595f933f
Rafael Pereira Dias <br.rafaeldias@gmail.com>
2022-09-14 17:34:31 -0300

    solução do exercício 1

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1) Observe o trecho de código abaixo:
+
+//	int INDICE = 13, SOMA = 0, K = 0;
+
+//enquanto K < INDICE faça
+//	{
+//    K = K + 1;
+//    SOMA = SOMA + K;
+//}
+
+//imprimir(SOMA);
+
+//Ao final do processamento, qual será o valor da variável SOMA?
+
+
+// declaração das variáveis
+int indice = 13;
+int soma = 0;
+int k = 0;
+
+// teste lógico
+while (k < indice)
+{
+    k = k + 1;
+    soma = soma + k;
+}
+
+// resultado
+Console.WriteLine(soma);
+
+// Resposta: Soma = 91;
